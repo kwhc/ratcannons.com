dbccb6a98adaf790893bf42ac9a2a8036e852c79
Kent W <kent.wilhelm@gmail.com>
2014-08-11 17:05:02 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Brooklyn Rat Cannons Baseball Club</title>
+    <link href='http://fonts.googleapis.com/css?family=Oswald:400,700' rel='stylesheet' type='text/css' />
+    <link type="text/css" rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" />
+    <link type="text/css" rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.min.css" />
+    <link type="text/css" rel="stylesheet" href="styles/type.css" />
+    <link type="text/css" rel="stylesheet" href="styles/color.css" />
+    <link type="text/css" rel="stylesheet" href="styles/layout.css" />
+    <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-30111953-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</head>
+
+<body class="container">
+    <div id="container">
+        <div class="row" style="margin-top:20rem;background:none;">
+            <div id="logo" class="col-md-3" style="">X</div>
+            <div id="sup" class="col-md-9">
+                <div style="padding: 20px;">
+                    <h2 id="bk">Brooklyn</h2>
+                    <h2 id="rc">Rat Cannons</h2>
+                    <h2 id="hb">Hardcore Baseball</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.2.21/angular.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</body>
+</html>
